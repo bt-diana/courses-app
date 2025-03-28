9c3c70dee91508762413d5d2bc203b34f62d96da
Diana Butiakova <butiakova.diana@gmail.com>
2025-03-28 18:15:49 +0600

    feat: add initial Courses component structure

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -1,11 +1,27 @@
+import CoursesList from '../CoursesList/CoursesList';
+import SearchBar from '../SearchBar/SearchBar';
 import './Courses.css';
+import {
+  mockCurrentCoursesList,
+  mockedAuthorsList,
+} from '../../assets/data/mockCoursesList';
 
 const Courses = () => {
-  return (
-    <>
-      <div>Courses</div>
-    </>
-  );
+  if (mockCurrentCoursesList?.length) {
+    return (
+      <>
+        <div>Courses</div>
+        <SearchBar />
+        <CoursesList />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div>Courses(Empty)</div>
+      </>
+    );
+  }
 };
 
 export default Courses;
