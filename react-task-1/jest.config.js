3334dd2182b8fb59d65965b1841dd3da3c9f9b37
Diana Butiakova <butiakova.diana@gmail.com>
2025-03-28 16:24:26 +0600

    chore: lint, format and delete comments

--- react-task-1/jest.config.js
+++ react-task-1/jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-  testEnvironment: "node",
-  transform: { "^.+\\.(ts|tsx|js|jsx)?$": "ts-jest" }
+  testEnvironment: 'node',
+  transform: { '^.+\\.(ts|tsx|js|jsx)?$': 'ts-jest' },
 };
