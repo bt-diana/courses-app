b8c616d8b12b6604fa91ab10470b676ca270f93f
Diana Butiakova <butiakova.diana@gmail.com>
2025-03-29 20:35:52 +0600

    feat: add AddNewCourseButton component

---
+++ react-task-1/src/components/AddNewCourseButton/AddNewCourseButton.tsx
@@ -0,0 +1,8 @@
+import './AddNewCourseButton.css';
+import { Button } from 'antd';
+
+const AddNewCourseButton = () => {
+  return <Button>Add New Course</Button>;
+};
+
+export default AddNewCourseButton;
