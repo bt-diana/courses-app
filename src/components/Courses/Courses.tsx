081e64f8ecba7a6d52f0229035455adcb4d18754
Diana Butiakova <butiakova.diana@gmail.com>
2025-03-30 23:54:52 +0600

    fix: run linters

---
+++ src/components/Courses/Courses.tsx
@@ -0,0 +1,39 @@
+import './Courses.css';
+import SearchBar from '../SearchBar/SearchBar';
+import CoursesList from '../CoursesList/CoursesList';
+import AddNewCourseButton from '../AddNewCourseButton/AddNewCourseButton';
+import { Course } from '../../types';
+import { Button } from 'antd';
+
+interface CoursesProps {
+  courses: Course[];
+}
+
+const Courses = ({ courses }: CoursesProps) => {
+  if (courses?.length) {
+    return (
+      <div className="courses">
+        <div className="courses-options">
+          <SearchBar />
+          <AddNewCourseButton />
+        </div>
+        <CoursesList courses={courses} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="courses">
+        <div className="courses-empty">
+          <div>Your list is empty</div>
+          <div>
+            Please use &apos;Add New Course&apos; button to add your first
+            course
+          </div>
+          <AddNewCourseButton />
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Courses;
