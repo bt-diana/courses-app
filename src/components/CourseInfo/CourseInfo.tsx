7b749935b13a3824fe7d4e9d102bc3fa219a5f45
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-01 03:00:27 +0600

    feat: add back button to card info component

---
+++ src/components/CourseInfo/CourseInfo.tsx
@@ -0,0 +1,52 @@
+import { Card, Typography } from 'antd';
+import { Course } from '../../types';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+import './CourseInfo.css';
+
+interface CourseInfoProps {
+  courseData: Course;
+  closeCourse: () => void;
+}
+
+const CourseInfo = ({ courseData, closeCourse }: CourseInfoProps) => {
+  return (
+    <>
+      <div className="title">
+        <ArrowLeftOutlined className="back" onClick={closeCourse} />
+        <Typography.Title level={2}>{courseData.title}</Typography.Title>
+      </div>
+      <Card className="card-info">
+        <div className="card-info-content">
+          <div className="card-info-description">
+            <div className="description-title">Description:</div>
+            <div className="description-value">{courseData.description}</div>
+          </div>
+          <div className="card-info-params">
+            <div key="id" className="card-info-param">
+              <span className="info-param-title">ID:&#32;</span>
+              <span className="info-param-value">{courseData.id}</span>
+            </div>
+            <div key="authors" className="card-info-param">
+              <span className="info-param-title">Authors:&#32;</span>
+              <span className="info-param-value">
+                {courseData.authors.join(', ')}
+              </span>
+            </div>
+            <div key="duration" className="card-info-param">
+              <span className="info-param-title">Duration:&#32;</span>
+              <span className="info-param-value">{courseData.duration}</span>
+            </div>
+            <div key="creationDate" className="card-info-param">
+              <span className="info-param-title">Created:&#32;</span>
+              <span className="info-param-value">
+                {courseData.creationDate.split('/').join('.')}
+              </span>
+            </div>
+          </div>
+        </div>
+      </Card>
+    </>
+  );
+};
+
+export default CourseInfo;
