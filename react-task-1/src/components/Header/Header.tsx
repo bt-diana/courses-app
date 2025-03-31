c46ee68abe5f7e13a3ae1ae5948f68b2f03eff02
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-01 03:29:25 +0600

    fix: fix image uploading

--- react-task-1/src/components/Header/Header.tsx
+++ react-task-1/src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import './Header.css';
 import { Layout } from 'antd';
 import Account from '../Account/Account';
+import logo from '/src/assets/images/logo.svg';
 
 const Header = () => {
   return (
     <Layout.Header className="header">
-      <img src="/src/assets/images/logo.svg" alt="logo" />
+      <img src={logo} alt="logo" />
       <Account />
     </Layout.Header>
   );
