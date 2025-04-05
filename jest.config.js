e49d75a187de226757ae3effe19b74600a87809a
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-05 13:55:19 +0600

    chore: configure jest and rtl

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,18 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-  testEnvironment: 'node',
-  transform: { '^.+\\.(ts|tsx|js|jsx)?$': 'ts-jest' },
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src/'],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '^.+\\.(css|less)$': '<rootDir>/CSSStub.js',
+  },
 };
