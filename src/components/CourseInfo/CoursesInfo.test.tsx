a24613aa943b075e90416d965be64899bdd27574
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-05 15:18:11 +0600

    feat: update formatting on CourseInfo tests

--- src/components/CourseInfo/CoursesInfo.test.tsx
+++ src/components/CourseInfo/CoursesInfo.test.tsx
@@ -4,63 +4,60 @@ import { Course } from '../../types';
 import CourseInfo from './CourseInfo';
 
 const courseDataTemplate: Course = {
-  id: 'de5aaa59-90f5-4dbc-b8a9-aaf205c551ba',
-  title: 'JavaScript',
-  description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
-  creationDate: '08/03/2021',
-  duration: 160,
-  authors: ['Vasiliy Dobkin', 'Nicolas Kim', 'Anna Sidorenko'],
+  id: '',
+  title: '',
+  description: '',
+  creationDate: '',
+  duration: '',
+  authors: '',
 };
 
 describe('CourseInfo', () => {
   test('should render course title', () => {
     const title = 'JavaScript';
-    const mockCourseData = { ...courseDataTemplate, title };
+    const mockCourseData: Course = { ...courseDataTemplate, title };
     render(<CourseInfo courseData={mockCourseData} closeCourse={jest.fn()} />);
     expect(screen.getByText(title)).toBeInTheDocument();
   });
 
   test('should render course description', () => {
     const description = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`;
-    const mockCourseData = { ...courseDataTemplate, description };
+    const mockCourseData: Course = { ...courseDataTemplate, description };
     render(<CourseInfo courseData={mockCourseData} closeCourse={jest.fn()} />);
     expect(screen.getByText(description)).toBeInTheDocument();
   });
 
   test('should render course id', () => {
     const id = 'de5aaa59-90f5-4dbc-b8a9-aaf205c551ba';
-    const mockCourseData = { ...courseDataTemplate, id };
+    const mockCourseData: Course = { ...courseDataTemplate, id };
     render(<CourseInfo courseData={mockCourseData} closeCourse={jest.fn()} />);
     expect(screen.getByText(id)).toBeInTheDocument();
   });
 
   test('should render course creation date', () => {
-    const creationDate = '08/03/2021';
-    const creationDateFormatted = '08.03.2021';
-    const mockCourseData = { ...courseDataTemplate, creationDate };
+    const creationDate = '08.03.2021';
+    const mockCourseData: Course = { ...courseDataTemplate, creationDate };
     render(<CourseInfo courseData={mockCourseData} closeCourse={jest.fn()} />);
-    expect(screen.getByText(creationDateFormatted)).toBeInTheDocument();
+    expect(screen.getByText(creationDate)).toBeInTheDocument();
   });
 
   test('should render course duration', () => {
-    const duration = 160;
-    const durationFormatted = '2:40 hours';
-    const mockCourseData = { ...courseDataTemplate, duration };
+    const duration = '2:40 hours';
+    const mockCourseData: Course = { ...courseDataTemplate, duration };
     render(<CourseInfo courseData={mockCourseData} closeCourse={jest.fn()} />);
-    expect(screen.getByText(durationFormatted)).toBeInTheDocument();
+    expect(screen.getByText(duration)).toBeInTheDocument();
   });
 
   test('should render course authors', () => {
-    const authors = ['Vasiliy Dobkin', 'Nicolas Kim', 'Anna Sidorenko'];
-    const authorsFormatted = 'Vasiliy Dobkin, Nicolas Kim, Anna Sidorenko';
-    const mockCourseData = { ...courseDataTemplate, authors };
+    const authors = 'Vasiliy Dobkin, Nicolas Kim, Anna Sidorenko';
+    const mockCourseData: Course = { ...courseDataTemplate, authors };
     render(<CourseInfo courseData={mockCourseData} closeCourse={jest.fn()} />);
-    expect(screen.getByText(authorsFormatted)).toBeInTheDocument();
+    expect(screen.getByText(authors)).toBeInTheDocument();
   });
 
   test('should call closeCourse function on back button click', () => {
     const mockCloseCourse = jest.fn();
-    const mockCourseData = { ...courseDataTemplate };
+    const mockCourseData: Course = { ...courseDataTemplate };
     render(
       <CourseInfo courseData={mockCourseData} closeCourse={mockCloseCourse} />
     );
