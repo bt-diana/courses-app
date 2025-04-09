0665ae230227582d777a603e52529015bbd223cf
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-09 20:11:39 +0600

    style: update LoginForm style

--- react-task-2/src/components/LoginForm/LoginForm.tsx
+++ react-task-2/src/components/LoginForm/LoginForm.tsx
@@ -14,41 +14,43 @@ interface LoginFormProps {
 
 const LoginForm = ({ onFinish, onFinishFailed }: LoginFormProps) => {
   return (
-    <Card className="login-card">
-      <Form
-        className="login-form"
-        name="basic"
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-        layout="vertical"
-      >
-        <Typography.Title level={2}>Login</Typography.Title>
-        <Form.Item<FieldType>
-          className="login-form-field"
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: 'Please input your username!' }]}
+    <div className="login-container">
+      <Card className="login-card">
+        <Form
+          className="login-form"
+          name="basic"
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          layout="vertical"
         >
-          <Input />
-        </Form.Item>
+          <Typography.Title level={2}>Login</Typography.Title>
+          <Form.Item<FieldType>
+            className="login-form-field"
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input />
+          </Form.Item>
 
-        <Form.Item<FieldType>
-          className="login-form-field"
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
+          <Form.Item<FieldType>
+            className="login-form-field"
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-        <Form.Item label={null}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </Card>
+          <Form.Item label={null}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
   );
 };
 
