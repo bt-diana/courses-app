8cd6ca17faf76332411f3da5219ae5e246a56ed4
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-11 16:38:30 +0600

    feat: configure dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
   base: './',
+  define: {
+    'process.env.VITE_API_SECRET': JSON.stringify(process.env.VITE_API_SECRET),
+  },
 });
