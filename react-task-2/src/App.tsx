ac366b8f8e952cf93e34f97709dcad29b0fce756
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-14 12:50:22 +0600

    feat: create CourseInfoPage

--- react-task-2/src/App.tsx
+++ react-task-2/src/App.tsx
@@ -8,14 +8,13 @@ import {
   Navigate,
 } from 'react-router-dom';
 import Layout, { Content } from 'antd/es/layout/layout';
-import Loading from './components/Loading/Loading';
 import LoginPage from './pages/LoginPage';
 import CoursesPage from './pages/CoursesPage';
-import CourseInfo from './components/CourseInfo/CourseInfo';
+import CourseInfoPage from './pages/CourseInfoPage';
 import CourseAddPage from './pages/CourseAddPage';
 import CourseEditPage from './pages/CourseEditPage';
 import { User } from './types';
-import { Suspense, useState } from 'react';
+import { useState } from 'react';
 import CurrentUser from './contexts/currentUser';
 import SetCurrentUser from './contexts/setCurrentUser';
 
@@ -37,9 +36,9 @@ const App = () => {
                 />
                 <Route element={<AuthenticatedRoute />}>
                   <Route path="/courses" element={<CoursesPage />} />
-                  {/* <Route path="/courses/:id" element={<CourseInfo />} />
-              <Route path="/courses/add" element={<CourseAddPage />} />
-              <Route path="/courses/:id/edit" element={<CourseEditPage />} /> */}
+                  <Route path="/courses/:id" element={<CourseInfoPage />} />
+                  {/* <Route path="/courses/add" element={<CourseAddPage />} />
+                  <Route path="/courses/:id/edit" element={<CourseEditPage />} /> */}
                 </Route>
               </Routes>
             </Content>
---
+++ react-task-2/src/pages/CourseInfoPage.tsx
@@ -0,0 +1,34 @@
+import { Course } from '../types';
+import { useEffect, useState } from 'react';
+import Loading from '../components/Loading/Loading';
+import { useParams } from 'react-router-dom';
+import normalizeCourse from '../helpers/normalizeCourse';
+import getCourse from '../api/getCourse';
+import getAuthors from '../api/getAuthors';
+import CourseInfo from '../components/CourseInfo/CourseInfo';
+
+const CourseInfoPage = () => {
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [courseData, setCourseData] = useState<Course>();
+
+  useEffect(() => {
+    getCourse(id!)
+      .then((course) => {
+        getAuthors().then((authors) => {
+          setCourseData(normalizeCourse(course, authors));
+        });
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [id]);
+
+  return isLoading || courseData == null ? (
+    <Loading />
+  ) : (
+    <CourseInfo courseData={courseData} />
+  );
+};
+
+export default CourseInfoPage;
