242dc1886bee7417d29491cfc978cfecaaea3b94
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-14 12:52:35 +0600

    refactor: change getUser.tsx extention to ts

---
+++ src/api/getUser.ts
@@ -0,0 +1,21 @@
+const accessToken = localStorage.getItem('accessToken');
+
+const getUser = () =>
+  fetch('https://dummyjson.com/auth/me', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw Error(`${res.status}: ${res.statusText}`);
+      }
+    })
+    .then(({ firstName, lastName }) => {
+      return { firstName, lastName };
+    });
+
+export default getUser;
