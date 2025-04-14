b14af6254d87b8d9e246f06c4f7c1abdef578600
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-14 15:57:27 +0600

    feat: create new author input in AuthorsAddEdit component

--- react-task-2/src/components/AuthorsAddEdit/AuthorsAddEdit.tsx
+++ react-task-2/src/components/AuthorsAddEdit/AuthorsAddEdit.tsx
@@ -1,8 +1,12 @@
-import { Button, Card } from 'antd';
+import { Button, Card, Form, Input } from 'antd';
 import './AuthorsAddEdit.css';
 import { AuthorResource } from '../../types';
 import { PlusOutlined, DeleteOutlined, CloseOutlined } from '@ant-design/icons';
 import { useState } from 'react';
+
+type FieldType = {
+  name?: string;
+};
 
 interface AuthorsAddEditProps {
   initialCourseAuthors?: string[];
@@ -46,6 +50,25 @@ const AuthorsAddEdit = ({
             </div>
           )
         )}
+        <Form className="create-author-form">
+          <Form.Item<FieldType>
+            className="create-author-form-field"
+            name="name"
+            rules={[
+              { required: true, message: 'Name is required' },
+              {
+                type: 'string',
+                min: 2,
+                message: 'Name must be at least 2 characters',
+              },
+            ]}
+          >
+            <Input placeholder="Author's name" />
+          </Form.Item>
+          <Button type="primary" htmlType="submit">
+            Create author
+          </Button>
+        </Form>
       </Card>
       <Card title="Course Authors" className="course-authors">
         {courseAuthors.map((id) => (
