a55dea8a18f31007a635378e337486be2c475c39
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-15 20:24:54 +0600

    feat: create Error component

---
+++ react-task-3/src/components/Error/Error.tsx
@@ -0,0 +1,21 @@
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+import './Error.css';
+import { Flex, Typography } from 'antd';
+
+interface ErrorProps {
+  message: string;
+}
+
+const Error = ({ message }: ErrorProps) => {
+  return (
+    <Flex gap="middle" className='error-container' vertical>
+      <Flex gap="middle" className="error">
+        <Typography.Title level={2}><ExclamationCircleOutlined /> An error has occcured!</Typography.Title>
+        <Typography.Text>{message}</Typography.Text>
+        <Typography.Text>Try reloading the page.</Typography.Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Error;
