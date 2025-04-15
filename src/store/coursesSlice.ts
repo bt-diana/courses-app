641e606c4b0dbc6558d0ec8912bec0d933f8a04c
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-15 23:54:53 +0600

    feat: create addCourse and editCourse async thunks

--- src/store/coursesSlice.ts
+++ src/store/coursesSlice.ts
@@ -2,6 +2,8 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { CourseResource } from '../types';
 import { DataState, Status } from '../types';
 import getCourses from '../api/getCourses';
+import postCourse from '../api/postCourse';
+import putCourse from '../api/putCourse';
 
 type CoursesState = DataState<CourseResource[]>;
 
@@ -14,6 +16,32 @@ const fetchCourses = createAsyncThunk('courses/fetchCourses', async () => {
     return message;
   }
 });
+
+const addCourse = createAsyncThunk(
+  'courses/addCourse',
+  async (courseData: Omit<CourseResource, 'id'>) => {
+    try {
+      const res = await postCourse(courseData);
+      return res;
+    } catch (error) {
+      const message = error instanceof Error ? error.message : 'Unknown Error';
+      return message;
+    }
+  }
+);
+
+const editCourse = createAsyncThunk(
+  'courses/editCourse',
+  async (course: CourseResource) => {
+    try {
+      const res = await putCourse(course.id, course);
+      return res;
+    } catch (error) {
+      const message = error instanceof Error ? error.message : 'Unknown Error';
+      return message;
+    }
+  }
+);
 
 const initialState: CoursesState = {
   data: [],
@@ -37,9 +65,35 @@ const coursesSlice = createSlice({
       .addCase(fetchCourses.rejected, (state, action) => {
         state.status = Status.failed;
         state.error = action.error.message ?? 'Unknown Error';
+      })
+
+      .addCase(addCourse.pending, (state) => {
+        state.status = Status.loading;
+      })
+      .addCase(addCourse.fulfilled, (state, action) => {
+        state.status = Status.succeeded;
+        state.data.push(action.payload);
+      })
+      .addCase(addCourse.rejected, (state, action) => {
+        state.status = Status.failed;
+        state.error = action.error.message ?? 'Unknown Error';
+      })
+
+      .addCase(editCourse.pending, (state) => {
+        state.status = Status.loading;
+      })
+      .addCase(editCourse.fulfilled, (state, action) => {
+        state.status = Status.succeeded;
+        state.data = state.data.map((course) =>
+          course.id === action.payload.id ? action.payload : course
+        );
+      })
+      .addCase(editCourse.rejected, (state, action) => {
+        state.status = Status.failed;
+        state.error = action.error.message ?? 'Unknown Error';
       });
   },
 });
 
-export { fetchCourses };
+export { fetchCourses, addCourse, editCourse };
 export default coursesSlice.reducer;
