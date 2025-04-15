db9ea0433f1234a9b20342490e677f8394a0725d
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-15 20:39:05 +0600

    feat: fetch courses and authors on CoursesPage using async thunks

--- react-task-3/src/pages/CoursesPage.tsx
+++ react-task-3/src/pages/CoursesPage.tsx
@@ -1,47 +1,67 @@
 import Courses from '../components/Courses/Courses';
-import { Course } from '../types';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import Loading from '../components/Loading/Loading';
 import normalizeCourses from '../helpers/normalizeCourses';
 import DeleteCourse from '../contexts/deleteCurrentCourse';
-import getCourses from '../api/getCourses';
-import getAuthors from '../api/getAuthors';
-import deleteCourse from '../api/deleteCourse';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  AppDispatch,
+  getCourses,
+  getCoursesStatus,
+  getCoursesError,
+  getAuthors,
+  getAuthorsStatus,
+  getAuthorsError,
+} from '../store';
+import { fetchCourses } from '../store/coursesSlice';
+import { Status } from '../types';
+import { fetchAuthors } from '../store/authorsSlice';
+import { isFailed, isLoading } from '../helpers/status';
+import Error from '../components/Error/Error';
 
 const CoursesPage = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [сurrentCourses, setCurrentCourses] = useState<Course[]>();
+  const dispatch = useDispatch<AppDispatch>();
+
+  const courses = useSelector(getCourses);
+  const coursesStatus = useSelector(getCoursesStatus);
+  const coursesError = useSelector(getCoursesError);
+
+  const authors = useSelector(getAuthors);
+  const authorsStatus = useSelector(getAuthorsStatus);
+  const authorsError = useSelector(getAuthorsError);
 
   useEffect(() => {
-    getCourses()
-      .then((courses) => {
-        getAuthors().then((authors) => {
-          setCurrentCourses(normalizeCourses(courses, authors));
-        });
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
+    if (coursesStatus === Status.idle) {
+      dispatch(fetchCourses());
+    }
+  }, [coursesStatus, dispatch]);
+
+  useEffect(() => {
+    if (authorsStatus === Status.idle) {
+      dispatch(fetchAuthors());
+    }
+  }, [authorsStatus, dispatch]);
 
   const deleteCurrentCourse = (idToDelete: string) => {
-    setIsLoading(true);
-    deleteCourse(idToDelete)
-      .then(() => {
-        setCurrentCourses(
-          сurrentCourses?.filter(({ id }) => id !== idToDelete)
-        );
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+    // setIsLoading(true);
+    // deleteCourse(idToDelete)
+    //   .then(() => {
+    //     setCurrentCourses(
+    //       сurrentCourses?.filter(({ id }) => id !== idToDelete)
+    //     );
+    //   })
+    //   .finally(() => {
+    //     setIsLoading(false);
+    //   });
   };
 
-  return isLoading || сurrentCourses == null ? (
+  return [coursesStatus, authorsStatus].some(isFailed) ? (
+    <Error message={coursesError ?? authorsError!} />
+  ) : [coursesStatus, authorsStatus].some(isLoading) ? (
     <Loading />
   ) : (
     <DeleteCourse value={deleteCurrentCourse}>
-      <Courses courses={сurrentCourses} />
+      <Courses courses={normalizeCourses(courses, authors)} />
     </DeleteCourse>
   );
 };
