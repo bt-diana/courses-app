d9b5c636de55a89300b6dcf95e79b5813091a824
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-15 19:48:13 +0600

    feat: create fetchCourses async thunk

--- react-task-3/src/store/index.ts
+++ react-task-3/src/store/index.ts
@@ -10,3 +10,11 @@ const store = configureStore({
 });
 
 export default store;
+
+export type AppStore = typeof store;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
+
+export const getCourses = (state: RootState) => state.courses.data;
+export const getCoursesStatus = (state: RootState) => state.courses.status;
+export const getCoursesError = (state: RootState) => state.courses.error;
