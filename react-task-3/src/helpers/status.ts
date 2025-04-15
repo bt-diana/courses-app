6eb34b6322d73bcf3ff922827e32d69135f275f7
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-15 23:23:31 +0600

    fix: remove Status.idle from isLoading

--- react-task-3/src/helpers/status.ts
+++ react-task-3/src/helpers/status.ts
@@ -1,7 +1,6 @@
 import { Status } from '../types';
 
 export const isIdle = (value: Status) => value === Status.idle;
-export const isLoading = (value: Status) =>
-  value === Status.loading || value === Status.idle;
+export const isLoading = (value: Status) => value === Status.loading;
 export const isSucceeded = (value: Status) => value === Status.succeeded;
 export const isFailed = (value: Status) => value === Status.failed;
