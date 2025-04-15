c9a779812b330995906d408e44d5e19525b10841
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-15 19:48:13 +0600

    feat: create fetchCourses async thunk

--- src/store/coursesSlice.ts
+++ src/store/coursesSlice.ts
@@ -1,73 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { CourseResource } from '../types';
+import { DataState, Status } from '../types';
+import getCourses from '../api/getCourses';
 
-type CoursesState = {
-  data: CourseResource[];
-};
+type CoursesState = DataState<CourseResource[]>;
 
-type CoursesAction<T> = {
-  type: string;
-  payload: T;
-};
+const fetchCourses = createAsyncThunk('courses/fetchCourses', async () => {
+  try {
+    const res = await getCourses();
+    return res;
+  } catch (error) {
+    const message = error instanceof Error ? error.message : 'Unknown Error';
+    return message;
+  }
+});
 
 const initialState: CoursesState = {
-  data: [
-    {
-      id: 'de5aaa59-90f5-4dbc-b8a9-aaf205c551ba',
-      title: 'JavaScript',
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-      creationDate: '08/03/2021',
-      duration: 160,
-      authors: [
-        'f762978b-61eb-4096-812b-ebde22838167',
-        '27cc3006-e93a-4748-8ca8-73d06aa93b6d',
-      ],
-    },
-    {
-      id: 'b5630fcv-9bf7-4d39-b75a-3m5906fd0916',
-      title: 'React',
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      creationDate: '11/04/2025',
-      duration: 610,
-      authors: [
-        'f762978b-61eb-4096-812b-ebde22838167',
-        '095a1817-d45b-4ed7-9cf7-b2417bcbf748',
-      ],
-    },
-  ],
+  data: [],
+  status: Status.idle,
+  error: null,
 };
 
 const coursesSlice = createSlice({
   name: 'courses',
   initialState,
-  reducers: {
-    addCourse: (
-      state: CoursesState,
-      action: CoursesAction<Omit<CourseResource, 'id'>>
-    ) => {
-      const newCourse: CourseResource = {
-        id: 'test' + new Date().getTime(),
-        ...action.payload,
-      };
-
-      state.data.push(newCourse);
-    },
-    editCourse: (
-      state: CoursesState,
-      action: CoursesAction<CourseResource>
-    ) => {
-      const course = state.data.find(({ id }) => id === action.payload.id);
-      if (course) {
-        Object.assign(course, action.payload);
-      }
-    },
-    removeCourse: (state: CoursesState, action: CoursesAction<string>) => {
-      state.data = state.data.filter(({ id }) => id !== action.payload);
-    },
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchCourses.pending, (state) => {
+        state.status = Status.loading;
+      })
+      .addCase(fetchCourses.fulfilled, (state, action) => {
+        state.status = Status.succeeded;
+        state.data = action.payload;
+      })
+      .addCase(fetchCourses.rejected, (state, action) => {
+        state.status = Status.failed;
+        state.error = action.error.message ?? 'Unknown Error';
+      });
   },
 });
 
-export const { addCourse, editCourse, removeCourse } = coursesSlice.actions;
+export { fetchCourses };
 export default coursesSlice.reducer;
