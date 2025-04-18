ba166935e4ec91c7ee629bde08870a05ac96ca9e
Diana Butiakova <butiakova.diana@gmail.com>
2025-04-19 00:01:00 +0600

    refactor: move routers to separate component

--- react-task-3/src/App.tsx
+++ react-task-3/src/App.tsx
@@ -1,37 +1,15 @@
 import './App.css';
-import AuthenticatedRoute from './components/AuthenticatedRoute/AuthenticatedRoute';
-import {
-  HashRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import CoursesPage from './pages/CoursesPage';
-import CourseInfoPage from './pages/CourseInfoPage';
-import CourseAddPage from './pages/CourseAddPage';
-import CourseEditPage from './pages/CourseEditPage';
 import UserProvider from './components/UserProvider/UserProvider';
 import Layout from './components/Layout/Layout';
+import Router from './components/Router/Router';
 
 const App = () => {
   return (
-    <Router>
-      <UserProvider>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Navigate replace to="/courses" />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route element={<AuthenticatedRoute />}>
-              <Route path="/courses" element={<CoursesPage />} />
-              <Route path="/courses/:id" element={<CourseInfoPage />} />
-              <Route path="/courses/add" element={<CourseAddPage />} />
-              <Route path="/courses/:id/edit" element={<CourseEditPage />} />
-            </Route>
-          </Routes>
-        </Layout>
-      </UserProvider>
-    </Router>
+    <UserProvider>
+      <Layout>
+        <Router />
+      </Layout>
+    </UserProvider>
   );
 };
 
