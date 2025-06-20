e8220fc5987b0dc20e981b7afee49fef7b1ecb0c
Diana Butiakova <butiakova.diana@gmail.com>
2025-06-21 00:14:45 +0600

    refactor: gather env variables declaration together

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,13 @@ import authorsRouter from './routes/authors.js';
 import cors from 'cors';
 import 'dotenv/config';
 
-const app = express();
 const port = process.env.PORT;
 const coursesPath = process.env.API_COURSES_PATH!;
 const authorsPath = process.env.API_AUTHORS_PATH!;
 const appURL = process.env.APP_URL!;
 const appDevURL = process.env.APP_DEV_URL!;
+
+const app = express();
 const corsOptions = {
     origin: [appURL, appDevURL],
 };
