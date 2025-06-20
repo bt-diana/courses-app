c98c8b2854b214a1d1ec2abb785aac6a172e1c5d
Diana Butiakova <butiakova.diana@gmail.com>
2025-06-20 21:20:15 +0600

    feat: configure cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,20 @@
 import express from 'express';
 import coursesRouter from './routes/courses.js';
 import authorsRouter from './routes/authors.js';
+import cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT;
 const coursesPath = process.env.API_COURSES_PATH!;
 const authorsPath = process.env.API_AUTHORS_PATH!;
+const appURL = process.env.APP_URL!;
+const appDevURL = process.env.APP_DEV_URL!;
+const corsOptions = {
+    origin: [appURL, appDevURL],
+};
 
+app.use(cors(corsOptions));
 app.use(coursesPath, coursesRouter);
 app.use(authorsPath, authorsRouter);
 
