fdd48fd89b8cdca80099ab874fc4bce7bb18242e
Diana Butiakova <butiakova.diana@gmail.com>
2025-06-22 17:25:50 +0600

    feat: create author post route

--- backend/src/routes/authors.ts
+++ backend/src/routes/authors.ts
@@ -4,6 +4,14 @@ import { Author } from '../entity/Author.js';
 
 const router = express.Router();
 
+const getAuthorById = async (id: string) => {
+    const author = await AppDataSource.manager
+        .createQueryBuilder(Author, 'author')
+        .where('author.id = :id', { id: id })
+        .getOne();
+    return author;
+};
+
 router.get('/', async (_, res) => {
     res.type('json');
     res.send(await AppDataSource.manager.find(Author));
@@ -11,11 +19,27 @@ router.get('/', async (_, res) => {
 
 router.get('/:id', async (req, res) => {
     res.type('json');
-    const author = await AppDataSource.manager
-        .createQueryBuilder(Author, 'author')
-        .where('author.id = :id', { id: req.params.id })
-        .getOne();
-    res.send(author);
+    res.send(await getAuthorById(req.params.id));
+});
+
+router.post('/', async (req, res) => {
+    const newAuthorId = (
+        await AppDataSource.manager
+            .createQueryBuilder()
+            .insert()
+            .into(Author)
+            .values(req.body)
+            .execute()
+    ).identifiers[0]?.id;
+    if (newAuthorId) {
+        res.status(201);
+        res.type('json');
+        res.send(await getAuthorById(newAuthorId));
+    } else {
+        res.status(400);
+        res.type('text');
+        res.send('Could not create an author');
+    }
 });
 
 export default router;
