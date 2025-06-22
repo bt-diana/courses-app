effed129eebd04f849443131be24a4600501b453
Diana Butiakova <butiakova.diana@gmail.com>
2025-06-22 18:24:04 +0600

    refactor: use crud functions for authors routes

--- backend/src/routes/authors.ts
+++ backend/src/routes/authors.ts
@@ -1,40 +1,27 @@
 import express from 'express';
-import AppDataSource from '../dataSource.js';
-import { Author } from '../entity/Author.js';
+import { readById, readAll } from '../crud/read.js';
+import { createAuthor } from '../crud/create.js';
+import { Author } from 'entity/Author.js';
 
 const router = express.Router();
 
-const getAuthorById = async (id: string) => {
-    const author = await AppDataSource.manager
-        .createQueryBuilder(Author, 'author')
-        .where('author.id = :id', { id: id })
-        .getOne();
-    return author;
-};
-
 router.get('/', async (_, res) => {
     res.type('json');
-    res.send(await AppDataSource.manager.find(Author));
+    res.send(await readAll());
 });
 
 router.get('/:id', async (req, res) => {
     res.type('json');
-    res.send(await getAuthorById(req.params.id));
+    res.send(await readById(Author, req.params.id));
 });
 
 router.post('/', async (req, res) => {
-    const newAuthorId = (
-        await AppDataSource.manager
-            .createQueryBuilder()
-            .insert()
-            .into(Author)
-            .values(req.body)
-            .execute()
-    ).identifiers[0]?.id;
-    if (newAuthorId) {
+    const author = createAuthor(Author, req.body);
+
+    if (author) {
         res.status(201);
         res.type('json');
-        res.send(await getAuthorById(newAuthorId));
+        res.send(author);
     } else {
         res.status(400);
         res.type('text');
