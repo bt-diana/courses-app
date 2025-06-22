e66b068cc12017360152d358f204af00c75e9d9b
Diana Butiakova <butiakova.diana@gmail.com>
2025-06-22 22:13:19 +0600

    feat: create course routes

--- backend/src/routes/courses.ts
+++ backend/src/routes/courses.ts
@@ -1,21 +1,72 @@
 import express from 'express';
-import AppDataSource from '../dataSource.js';
 import { Course } from '../entity/Course.js';
+import { readInstance, readInstanceAll } from '../crud/read.js';
+import { createInstance } from '../crud/create.js';
+import { updateInstance } from '../crud/update.js';
+import { deleteInstance } from '../crud/delete.js';
 
 const router = express.Router();
 
 router.get('/', async (_, res) => {
     res.type('json');
-    res.send(await AppDataSource.manager.find(Course));
+    res.send(await readInstanceAll(Course));
 });
 
 router.get('/:id', async (req, res) => {
-    res.type('json');
-    const course = await AppDataSource.manager
-        .createQueryBuilder(Course, 'course')
-        .where('course.id = :id', { id: req.params.id })
-        .getOne();
-    res.send(course);
+    const course = await readInstance(Course, req.params.id);
+
+    if (course) {
+        res.type('json');
+        res.send(course);
+    } else {
+        res.status(404);
+        res.type('text');
+        res.send('Could not find course with id=' + req.params.id);
+    }
+});
+
+router.post('/', async (req, res) => {
+    const course = await createInstance(Course, req.body);
+
+    if (course) {
+        res.status(201);
+        res.type('json');
+        res.send(course);
+    } else {
+        res.status(400);
+        res.type('text');
+        res.send('Wrong request body');
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const course = await updateInstance(Course, req.params.id, req.body);
+
+    if (course) {
+        res.type('json');
+        res.send(course);
+    } else {
+        res.status(404);
+        res.type('text');
+        res.send('Could not find course with id=' + req.params.id);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    if (await readInstance(Course, req.params.id)) {
+        if (await deleteInstance(Course, req.params.id)) {
+            res.status(204);
+            res.end();
+        } else {
+            res.status(400);
+            res.type('text');
+            res.send('Could not delete course with id=' + req.params.id);
+        }
+    } else {
+        res.status(404);
+        res.type('text');
+        res.send('Could not find course with id=' + req.params.id);
+    }
 });
 
 export default router;
