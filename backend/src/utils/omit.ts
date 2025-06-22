6801ea82a19c8a0639762aaa029b238e9c8d049f
Diana Butiakova <butiakova.diana@gmail.com>
2025-06-22 23:43:36 +0600

    chore: disabele @typescript-eslint/no-unused-vars rule for omit function

--- backend/src/utils/omit.ts
+++ backend/src/utils/omit.ts
@@ -1,4 +1,5 @@
 const omit = <T extends object>(obj: T, key: keyof T) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { [key]: ommited, ...rest } = obj;
     return rest;
 };
