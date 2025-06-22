b157e5f151e206416e8a29ba9881dd400ccb81ee
Diana Butiakova <butiakova.diana@gmail.com>
2025-06-22 22:30:07 +0600

    feat: create getRelatedData function

--- backend/src/crud/read.ts
+++ backend/src/crud/read.ts
@@ -1,6 +1,19 @@
 import { ObjectLiteral, EntityTarget } from 'typeorm';
 import AppDataSource from '../dataSource.js';
 
+const getRelatedData = <T extends ObjectLiteral>(
+    entity: EntityTarget<T>,
+    relation: string,
+    instance: T,
+) => {
+    return AppDataSource.createQueryBuilder()
+        .relation(entity, relation)
+        .of(instance)
+        .loadMany();
+};
+
+export { getRelatedData };
+
 const readInstance = <T extends ObjectLiteral>(
     entity: EntityTarget<T>,
     id: string,
@@ -15,4 +28,4 @@ const readInstanceAll = <T extends ObjectLiteral>(entity: EntityTarget<T>) => {
     return AppDataSource.manager.find(entity);
 };
 
-export { readInstance, readInstanceAll };
+export { readInstance, readInstanceAll, getRelatedData };
